710e7c7fe393e5272d2b3b4fd82000dba86789bf
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-07-29 08:16:01 +0300

    done22

--- js/task-5.js
+++ js/task-5.js
@@ -1,13 +1,18 @@
-import users from './users.js'
+// import users from './users.js'
 
 
 
-const getUserWithEmail = (users, email) => {
-  return users.find(user => user.email === email)
-};
+// const getUserWithEmail = (users, email) => {
+//   return users.find(user => user.email === email)
+// };
 
 
 
   
-  console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
-  console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+//   console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+//   console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+
+
+
+
+
--- js/task-9.js
+++ js/task-9.js
@@ -1,15 +1,19 @@
 import users from './users.js'
 
 
+// const getNamesSortedByFriendsCount = users => {
+
+//    return users.slice().sort((a, b) => a.friends.length - b.friends.length).map(user => user.name)
+  
+// };
+
+
+
 const getNamesSortedByFriendsCount = users => {
 
-   return users.reduce((acc, user) => {
-      acc.push({name: user.name, friends: user.friends})
-      return acc
-   }, []).sort((a, b) => a.friends.length - b.friends.length).map(user => user.name)
+   return [...users].sort((a, b) => a.friends.length - b.friends.length).map(user => user.name)
   
 };
-
 
 
 console.log(getNamesSortedByFriendsCount(users));
